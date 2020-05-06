1ea4c09c49bc73e0c0974bfecd3865ea0781b50c
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-07 04:29:27 +0700

    #2 - Initial Project for spring-security-sso-auth-server

---
+++ spring-security-sso-auth-server/src/main/java/com/hendisantika/springsecurityssoauthserver/SpringSecuritySsoAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springsecurityssoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySsoAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySsoAuthServerApplication.class, args);
+    }
+
+}
---
+++ spring-security-sso-auth-server/src/test/java/com/hendisantika/springsecurityssoauthserver/SpringSecuritySsoAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springsecurityssoauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySsoAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
