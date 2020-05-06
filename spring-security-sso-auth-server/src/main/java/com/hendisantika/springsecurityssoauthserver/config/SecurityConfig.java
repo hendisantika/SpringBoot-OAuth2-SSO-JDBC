61e6ca9cdcb587869d8ba418ae82762ec43874cb
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-07 05:15:08 +0700

    #11 - Add Configure & Password Bean in SecurityConfig for spring-security-sso-auth-server

--- spring-security-sso-auth-server/src/main/java/com/hendisantika/springsecurityssoauthserver/config/SecurityConfig.java
+++ spring-security-sso-auth-server/src/main/java/com/hendisantika/springsecurityssoauthserver/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springsecurityssoauthserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -22,4 +26,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+        http.requestMatchers()
+                .antMatchers("/login", "/oauth/authorize")
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .usernameParameter("username").passwordParameter("password")
+                .permitAll()
+                .and().csrf().disable();
+    } // @formatter:on
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off
+//        auth.inMemoryAuthentication()
+//            .withUser("henry")
+//            .password(passwordEncoder().encode("123"))
+//            .roles("USER");
+
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled"
+                        + " from users where username=?")
+                .authoritiesByUsernameQuery("select username, authority "
+                        + "from authorities where username=?")
+                .passwordEncoder(passwordEncoder());
+    } // @formatter:on
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
