75de070b513fe8e868bed98b6a4c21a9d5f36b53
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-07 05:30:30 +0700

    #20 - Add UiWebConfig for spring-security-sso-ui

---
+++ spring-security-sso-ui/src/main/java/com/hendisantika/springsecurityssoui/UiWebConfig.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springsecurityssoui;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-sso-ui
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/05/20
+ * Time: 05.30
+ */
+@Configuration
+@EnableWebMvc
+public class UiWebConfig implements WebMvcConfigurer {
+}
