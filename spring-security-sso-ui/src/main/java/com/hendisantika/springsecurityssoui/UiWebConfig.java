8e8685b3648be6f6557cf6076cde0ab9305bf288
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-07 05:30:52 +0700

    #21 - Add propertySourcesPlaceholderConfigurer Bean in UiWebConfig for spring-security-sso-ui

--- spring-security-sso-ui/src/main/java/com/hendisantika/springsecurityssoui/UiWebConfig.java
+++ spring-security-sso-ui/src/main/java/com/hendisantika/springsecurityssoui/UiWebConfig.java
@@ -1,6 +1,8 @@
 package com.hendisantika.springsecurityssoui;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,4 +18,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class UiWebConfig implements WebMvcConfigurer {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
 }
