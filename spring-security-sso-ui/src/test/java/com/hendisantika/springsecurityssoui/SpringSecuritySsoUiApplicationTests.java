b9df8fad1d407447e25086e0dc47077322c86a29
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-07 05:24:24 +0700

    #14 - Initial Commit for spring-security-sso-ui

---
+++ spring-security-sso-ui/src/test/java/com/hendisantika/springsecurityssoui/SpringSecuritySsoUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springsecurityssoui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySsoUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
